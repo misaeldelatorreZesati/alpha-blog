2f4de3f5a4cefed68fbe77775a110814faa8d20b
misaeldelatorreZesati <misael.delatorre@zesatiinternacional.com>
2024-02-07 14:49:08 -0600

    Make app production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
     def show
-        @article = Article.find(params[:id])
     end
 
     def index
@@ -12,11 +12,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
           flash[:notice] = "Article created successfully"
           redirect_to @article
@@ -27,14 +26,30 @@ class ArticlesController < ApplicationController
     end
 
     def update
-      @article = Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
+      if @article.update(article_params)
         flash[:notice] = "Article updated successfully"
         redirect_to @article
       else
         render :edit, status: :unprocessable_entity
       end 
     end
+
+    def destroy
+      @article.destroy
+      flash[:notice] = "Article deleted successfully"
+      redirect_to articles_path, notice: "Article deleted successfully", status: :see_other
+    end
+
+    private
+
+    def set_article
+    @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
 
 
 
