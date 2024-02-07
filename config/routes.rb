05ce23e6a4eec3240ce16412681121eedacaae6c
misaeldelatorreZesati <misael.delatorre@zesatiinternacional.com>
2024-02-07 09:24:01 -0600

    Add Create and update methods to article_controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
 
   get 'about',to: 'pages#about'
 
-  resources :articles, only: [:show, :index]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
